9f3663fb86c08c9b68898ae6964a612f778f5f06
Roman92P <roman.paszkow@gmail.com>
2023-07-27 10:43:07 +0200

    added main class, excluded previousl

---
+++ YCM_API/src/main/java/com/pashkov/ycm/ycm_api/YcmApiApplication.java
@@ -0,0 +1,13 @@
+package com.pashkov.ycm.ycm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcmApiApplication.class, args);
+    }
+
+}
